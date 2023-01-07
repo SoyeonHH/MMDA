5a63420a7b40d9bc7e5b996a7faba9d418eccd46
SoyeonHH <sodus1102@gmail.com>
2023-01-07 12:54:07 +0900

    Fix the gpu configuration

--- src/utils/convert.py
+++ src/utils/convert.py
@@ -1,10 +1,12 @@
 import torch
+from config import get_config
 
 def to_gpu(x, on_cpu=False, gpu_id=None):
     """Tensor => Variable"""
     if torch.cuda.is_available() and not on_cpu:
         # x = x.cuda(gpu_id)
-        device = torch.device('cuda')
+        config = get_config()
+        device = torch.device(config.device)
         x = x.to(device)
     return x
 
