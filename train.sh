d3bc02f44ad6514c7541afcee23237c291c88f04
SoyeonHH <sodus1102@gmail.com>
2023-03-21 14:38:27 +0900

    Modify experimental training module

--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-python src/train.py --data mosei --eval_mode micro --learning_rate 1e-5 --dropout 0.6 --conf_lr 1e-5 --conf_dropout 0.5 --device cuda --batch_size 32
+python src/train.py --data mosei --eval_mode micro --learning_rate 1e-5 --dropout 0.6 --use_confidNet True --conf_lr 1e-5 --conf_dropout 0.6 --use_kt False --device cuda --batch_size 32
